d629c108be05f70bac318edc19ccee8080762132
Ángel Rafael Diaz Bahena <l21320990@cancun.tecnm.mx>
2024-11-18 18:54:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mis practicas HTML :3 </title>
+<title>Mis practicas HTML :3 </title>
 </head>
 <body>
     <h1>Mis practicas HTML :3 </h1>
     <ul>
-        <li><a href="https://arxfa27.github.io/paola.oi/" target="_blank">practica 2</a></li>
-        <li><a href="https://arxfa27.github.io/Rafael-Diaz/" target="_blank">practica 1</a></li>
-        <li><a href="https://arxfa27.github.io/Listas/" target="_blank">practica 3</a></li>
-         <li><a href="https://arxfa27.github.io/practica-4/" target="_blank">practica 4</a></li>
-        <li><a href="https://arxfa27.github.io/practica5/" target="_blank">practica 5</a></li>
-        <li><a href="https://arxfa27.github.io/practica-6/" target="_blank">practica 6</a></li>
-        <li><a href="http://127.0.0.1/prog/practica%201.php" target="_blank">practica 1 php</a></li>
-         <li><a href="http://127.0.0.1/prog/ptactica2.php" target="_blank">practica 2 php</a></li>
-         <li><a href="http://127.0.0.1/prog/practica%203.php" target="_blank">practica 3 php</a></li>
-            <li><a href="http://127.0.0.1/prog/practica%204.php" target="_blank">practica 4 php</a></li>
-            <li><a href="http://127.0.0.1/prog/practica%205.php" target="_blank">practica 5 php</a></li>
+        <li><a href="" target="_blank">practica 2</a></li>
+        <li><a href="practica/paola1.html" target="_blank">practica 1</a></li>
+        <li><a href="practica/PAOLA 2.html" target="_blank">practica 2</a></li>
+         <li><a href="practica/Listas.html" target="_blank">practica 3</a></li>
+          <li><a href="practica/Practica 4.html" target="_blank">practica 3</a></li>
+        <li><a href="practica/practica 5.html" target="_blank">practica 5</a></li>
+        <li><a href="practica/PRACTICA 6.html" target="_blank">practica 6</a></li>
+        <li><a href="practica/conexion.php" target="_blank">practica 1 php</a></li>
+         <li><a href="practica/creacion bd.php" target="_blank">practica 7 php</a></li>
+         <li><a href="practica/practica 1.php" target="_blank">practica 1 php</a></li>
+            <li><a href="practica/ptactica2.php" target="_blank">practica 2 php</a></li>
+            <li><a href="practica/practica 3.php" target="_blank">practica 3 php</a></li>
+            <li><a href="practica/practica 4.php" target="_blank">practica 4 php</a></li>
+            <li><a href="practica/practica 5.php" target="_blank">practica 5 php</a></li>
+            <li><a href="practica/practica6.php" target="_blank">practica 6php</a></li>
         
         
         
