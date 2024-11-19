0bdd30cbdc6f95ff1c529dd0cc831345c19e3c69
Ángel Rafael Diaz Bahena <l21320990@cancun.tecnm.mx>
2024-11-18 19:02:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,28 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Tareas Vinculo</title>
-    </head>
-        <body>
-            <h1 style="text-align: center;">Esta es mi biblioteca de Actividades Diaz Bahena</h1>
-            <a href="paola1.html" title="Practica 1"> Práctica 1 </a>
-            <br>
-            <a href="PAOLA2.html" title="Práctica 2"> Práctica 2 </a>
-            <br>
-            <a href="Listas.html" title="Práctica 3"> Práctica 3 </a>
-            <br>
-            <a href="Practica 4.html" title="Práctica 4"> Práctica 4 </a>
-            <br>
-            <a href="practica 5.html" title="Práctica 5"> Práctica 5 </a>
-             <br>
-            <a href="PRACTICA 6.html" title="Práctica 6"> Práctica 6 </a>
-            <br>
-
-            <p>Praácticas de PHP en mi nuevo link</p>
-            <br>
-             <a href="practica 1.php" title="Práctica 1"> Práctica 1 </a>
-            <br>
-            <a href="ptactica2.php" title="Práctica 2"> Práctica 2 </a>
-            <br>
-             <a href="practica 3.php" title="Práctica 3"> Práctica 3 </a>
-            <br>
-             <a href="practica 4.php" title="Práctica 4"> Práctica 4 </a>
-            <br>
-             <a href="practica 5.php" title="Práctica 5"> Práctica 5 </a>
-            <br>
-             <a href="practica6.php" title="Práctica 6"> Práctica 6 </a>
-            <br>
-             <a href="conexion.jpg" title="Práctica 7"> Práctica 7 </a>
-        </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis practicas HTML :3 </title>
+</head>
+<body>
+    <h1>Mis practicas HTML :3 </h1>
+    <ul>
+        <li><a href="https://arxfa27.github.io/paola.oi/" target="_blank">practica 2</a></li>
+        <li><a href="https://arxfa27.github.io/Rafael-Diaz/" target="_blank">practica 1</a></li>
+        <li><a href="https://arxfa27.github.io/Listas/" target="_blank">practica 3</a></li>
+         <li><a href="https://arxfa27.github.io/practica-4/" target="_blank">practica 4</a></li>
+        <li><a href="https://arxfa27.github.io/practica5/" target="_blank">practica 5</a></li>
+        <li><a href="https://arxfa27.github.io/practica-6/" target="_blank">practica 6</a></li>
+        <li><a href="http://127.0.0.1/prog/practica%201.php" target="_blank">practica 1 php</a></li>
+         <li><a href="http://127.0.0.1/prog/ptactica2.php" target="_blank">practica 2 php</a></li>
+         <li><a href="http://127.0.0.1/prog/practica%203.php" target="_blank">practica 3 php</a></li>
+            <li><a href="http://127.0.0.1/prog/practica%204.php" target="_blank">practica 4 php</a></li>
+            <li><a href="http://127.0.0.1/prog/practica%205.php" target="_blank">practica 5 php</a></li>
+        
+        
+        
+        
+    </ul>
+</body>
 </html>
